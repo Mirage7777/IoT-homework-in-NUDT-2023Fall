74ed497f8ac0f9fb4b1f3d8ec71a0824adca899f
Mirage7777 <86822885+Mirage7777@users.noreply.github.com>
2023-09-20 16:49:10 +0800

    Update client.py

--- exp2/client.py
+++ exp2/client.py
@@ -6,10 +6,8 @@ import sys
 basicConfig(level=DEBUG)  # set now for dtls import code
 from dtls import do_patch
 
-
 do_patch()
 blocksize = 1024
-
 
 def main():
     cert_path = path.join(path.abspath(path.dirname(__file__)), "certs")
@@ -17,38 +15,40 @@ def main():
                         ca_certs=path.join(cert_path, "ca-cert.pem"))
     s.connect(('127.0.0.1', 28000))
     # 发送文件
-    while True:
-        print("input ls <dir> to list files in dir.\n")
-        print("input get <filename> to get file from dir.\n")
-        send_msg = raw_input(">")
-        cmd,filename = send_msg.split(" ")
-        try:
-            s.send(send_msg.encode())
-        except Exception as e:
-            print("[-] Can not send Data")
-        try:
-            if cmd == "ls":
-                data = s.recv(blocksize)
-                print(data.decode())
-            else:
-                filename = filename.split("/")[-1]
-                filedir = "./"+filename
-                with open(filedir,"wb") as fd:
-                    while True:
-                        data = s.recv(blocksize)
-                        if data.decode() == "Already Send":
-                            print("Already Receive")
-                            break
-                        fd.write(data)
-        except Exception as e:
-            print("[-] Can not receive Data")
-
-
+    try:
+        while True:
+            print("input ls <dir> to list files in dir.\n")
+            print("input get <filename> to get files from dir.\n")
+            send_msg = input(">")
+            cmd,filename = send_msg.split(" ")
+            try:
+                s.send(send_msg.encode())
+            except Exception as e:
+                print("[-]Can not send Data")
+            try:
+                if cmd == "ls":
+                    data = s.recv(blocksize)
+                    print(data.decode())
+                else:
+                    filename = filename.split("/")[-1]
+                    filedir = "./" + filename
+                    with open(filedir,"wb") as fd :
+                        while True:
+                            data = s.recv(blocksize)
+                            if data.decode() == "Already Send":
+                                print("Already Reveive.")
+                                break
+                            fd.write(data)
     
+            except Exception as e:
+                print("[-]Can not receive Data")
+    except KeyboardInterrupt:
+        s.close()
+        sys.exit(0)
 
 
 if __name__ == "__main__":
     main()
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(('127.0.0.1', 28000))
+    # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # s.connect(('127.0.0.1', 28000))
 
